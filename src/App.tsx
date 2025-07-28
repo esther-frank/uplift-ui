ca944f8ca645e4632a6871ffb5a6f7cc2bf89a42
esther-frank <185348393+esther-frank@users.noreply.github.com>
2025-07-28 16:10:15 +0100

    bootstrap setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
+import './App.scss'
+import { Card } from 'react-bootstrap'
 
 function App() {
   return (
     <>
       <p>Hello, world</p>
+      <Card>Hello again world</Card>
     </>
   )
 }
