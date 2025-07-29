1c9e3f01c75864817220feeac8f9a0c4937f4eb5
esther-frank <185348393+esther-frank@users.noreply.github.com>
2025-07-29 15:24:17 +0100

    merge conflicts again

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 
 import './App.scss'
 import Login from './pages/Login/Login'
+import Login from './pages/Login/Login'
 import Reflections from './pages/Reflections'
 import ReflectionPage from './pages/ReflectionPage'
-import Home from './pages/Home/Home'
-import Navbar from './Components/Navbar/Navbar'
+import Home from './pages/Home/Home/Home'
+import Navbar from './Components/Navbar/Navbar/Navbar'
 
 function App() {
   const [userId, setUserId] = useState<string | null>('123')
   const [showNavItems, setShowNavItems] = useState<boolean>(false)
+  const [showNavItems, setShowNavItems] = useState<boolean>(false)
   return (
     <Router>
       <Navbar showItems={showNavItems} />
+      <Navbar showItems={showNavItems} />
       <Routes>
+        <Route path="/pages/home" element={<Home userId={userId} />} />
+        <Route
+          path="/"
+          element={
+            <Login
+              userId={userId}
+              setuserId={setUserId}
+              setShowNavItems={setShowNavItems}
+            />
+          }
+        />
+        <Route
+          path="/pages/reflections"
+          element={<Reflections userId={userId} />}
+        />
         <Route path="/pages/home" element={<Home userId={userId} />} />
         <Route
           path="/"
